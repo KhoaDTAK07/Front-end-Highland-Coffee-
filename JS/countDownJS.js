394e7182d1c603fa9b53ab6a889ad84d54e2f4ae
KhoaDTAK07 <53048122+KhoaDTAK07@users.noreply.github.com>
2021-08-17 21:39:02 +0700

    done Count Down Timer project

---
+++ JS/countDownJS.js
@@ -0,0 +1,36 @@
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+
+const newYears = '01/01/2022';
+
+function countDown() {
+    const newYearDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    console.log(days);
+
+    daysElement.innerHTML = formatTime(days);
+    hoursElement.innerHTML = formatTime(hours);
+    minutesElement.innerHTML = formatTime(minutes);
+    secondsElement.innerHTML = formatTime(seconds);
+
+    // console.log(days, hours, minutes, seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+//initial call
+countDown();
+
+setInterval(countDown, 1000);
